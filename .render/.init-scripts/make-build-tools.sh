d1cded0fce71cf8216ce8bd705c6e55455068d98
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-10-06 18:55:39 +0100

    Put install-dependencies back

--- .render/.init-scripts/make-build-tools.sh
+++ .render/.init-scripts/make-build-tools.sh
@@ -3,3 +3,4 @@
 set -o errexit
 
 git clone --single-branch --branch deploy-action https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
+.scripts/install-dependencies.sh
